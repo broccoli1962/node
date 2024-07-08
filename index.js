1551674396cdfe465cd45d1955abee2fc12c61c7
broccoli1962 <aq0011@naver.com>
2024-07-08 18:53:24 +0900

    123
    123

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const { Client } = require("pg");
+const express = require('express');
+const app = express();
+
 const client = new Client({
   user: "root",
   host: "svc.sel5.cloudtype.app",
@@ -6,6 +9,7 @@ const client = new Client({
   password: "13241324",
   port: 32280,
 });
+
 client.connect();
 client.query("SELECT NOW()", (err, res) => {
   console.log(err, res);
@@ -13,4 +17,14 @@ client.query("SELECT NOW()", (err, res) => {
 client.query("SELECT * FROM rank", (err, res) => {
     console.log(err, res);
     client.end();
-})
+});
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`server running at http://localhost:${PORT}/`);
+});
