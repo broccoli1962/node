665e9a22e8e78a9b878159c5e7fc0cea6349335e
broccoli1962 <aq0011@naver.com>
2024-07-20 20:20:32 +0900

    123
    123

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.post('/ranking', async (req, res) => {
+app.get('/ranking', async (req, res) => {
   const data = req.body;
   const score = data.score;
   const playerName = data.playerName;
