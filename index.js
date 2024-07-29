84034ae2232aeda7eb95cdc4aa88e1ef7561d637
broccoli1962 <aq0011@naver.com>
2024-07-29 19:56:44 +0900

    유니티에서 데이터 받기
    유니티에서 데이터 받기

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client } = require("pg");
 const express = require('express');
+const multer = require('multer');
 const app = express();
 
 const client = new Client({
@@ -12,37 +13,42 @@ const client = new Client({
 
 client.connect();
 client.query("SELECT NOW()", (err, res) => {
-  console.log(err, res);
+  // console.log(err, res);
 });
 client.query("SELECT * FROM rank", (err, res) => {
-    console.log(err, res);
-    client.end();
+  // console.log(err, res);
+  client.end();
 });
 
 const PORT = 3000;
 
+const storage = multer.memoryStorage(); // 메모리 저장소를 사용
+const upload = multer({ storage: storage });
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+  res.send('Hello, World!');
 });
 
-app.get('/ranking', async (req, res) => {
-  const data = req.body;
-  const score = data.score;
-  const playerName = data.playerName;
+app.post('/ranking', upload.none(), async (req, res) => {
+  try {
+    console.log(req.body);
+    let playerName = req.body.playerName;
+    let score = parseInt(req.body.score);
+    console.log('playerName :', playerName);
+    console.log('score :', score);
 
-  try{
-    await client.connect();
-    const query = 'INSERT INTO rank (player_id , score) VALUES ($1, $2)';
-    const values = [playerName, score];
-    await client.query(query, values);
-    await client.end();
+    // await client.connect();
+    // const query = 'INSERT INTO rank (player_id, score) VALUES ($1, $2)';
+    // const values = [playerName, score];
+    // await client.query(query, values);
+    // await client.end();
     res.status(200).send('rank data saved');
-  }catch(err){
+  } catch (err) {
     console.error(err);
     res.status(500).send('error rank data saved');
   }
 });
 
 app.listen(PORT, () => {
-    console.log(`server running at http://localhost:${PORT}/`);
+  console.log(`server running at http://localhost:${PORT}/`);
 });
