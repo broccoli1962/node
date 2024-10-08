40177cbbbeeed53ce2c3d8ec1f0c8140fea33e5c
broccoli <aq0011@naver.com>
2024-10-09 00:09:26 +0900

    포트폴리오용 웹페이지

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const rank = require("./routes/rank");
-const router = require("./routes/index");
+const home = require("./routes/blog");
 const database = require("./routes/database");
 // app.use(express.json);
 app.engine('html', require('ejs').renderFile);
@@ -12,8 +12,8 @@ const PORT = 3000;
 
 //랭킹보드용 테스트
 app.use(rank);
-
-app.use('/', router);
+//웹 페이지용 테스트
+app.use(home);
 
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}/`);
