e487cbfb26c871492d3918e79945d588d99aa391
broccoli <aq0011@naver.com>
2024-10-08 22:20:00 +0900

    test
    tset

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/testg', function(req, res, next) {
-  res.render('index.html');
+  res.render('./views/index.html');
 });
 
 module.exports = router;
