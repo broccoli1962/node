f11e11b9ab6729ea83a39deac53c52edab84f8a1
broccoli1962 <aq0011@naver.com>
2025-01-12 23:45:15 +0900

    href
    href

--- public/routes/blog.js
+++ public/routes/blog.js
@@ -9,4 +9,16 @@ home.get('/pptpage', (req, res)=>{
   res.render('pptpage.html');
 });
 
+home.get('/management', (req, res) => {
+  res.render('management.html');
+});
+
+home.get('/debug', (req, res) => {
+  res.render('debug.html');
+});
+
+home.get('/download', (req, res) => {
+  res.render('download.html');
+});
+
 module.exports = home;
